d0cba3e9eb2f9605bc328ce23fa6041be70f95cd
mozukraken <mozukraken@gmail.com>
2018-04-25 01:49:27 +0900

    サービス画面からmicropostを投稿出来るようにした

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,16 +1,15 @@
 class StaticPagesController < ApplicationController
   def home
-    # app/views/リソース名/アクション名.html.erb
-    # app/views/static_pages/home.html.erb
+    @micropost = current_user.microposts.build if logged_in?
   end
 
   def help
   end
-  
+
   def about
     # 'app/views/static_pages/about.html.erb'
   end
-  
+
   def contact
     # app/views/static_pages/contact.html.erb'
   end
