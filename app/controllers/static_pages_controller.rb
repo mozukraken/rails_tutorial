22a6ad8eb5e77c8bfb978f021d04190631119efb
mozukraken <mozukraken@gmail.com>
2018-05-08 00:15:43 +0900

    Controllerで検索処理の作成

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,11 @@ class StaticPagesController < ApplicationController
   def home
     if logged_in?
       @micropost  = current_user.microposts.build
-      @feed_items = current_user.feed.paginate(page: params[:page])
+      if params[:search]
+        @feed_items = Micropost.where("microposts.content LIKE :search", search: "%#{params[:search]}%").paginate(page: params[:page])
+      else
+        @feed_items = current_user.feed.paginate(page: params[:page])
+      end
     end
   end
 
